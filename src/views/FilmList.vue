755d0f8625fe621863ad81bfab95a7462c27c556
Encouse <54453362+Encouse@users.noreply.github.com>
2020-05-18 11:27:27 +0300

    Changed to receive listoffilms

--- src/views/FilmList.vue
+++ src/views/FilmList.vue
@@ -1,1 +1,15 @@
 
+import { Component, Prop, Vue } from "vue-property-decorator"
+import Axios, { AxiosResponse } from "axios"
+import { BriefFilm } from "@/film-model"
+import { getFilmById } from "@/api/film"
+
+
+export default class FilmView extends Vue {
+    private filmEntity!: FilmModel
+    private dataReady = false
+    async mounted() {
+        this.filmEntity = await getListOfFilms()
+        this.dataReady = true
+    }
+}
